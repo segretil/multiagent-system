d2f5ea491d50e48b3091c869152450e8d4d95374
Boussant-roux Luc <L0calit@github.fr>
2017-10-23 23:54:00 +0200

    Rajout du rebond de la balle sur les murs

--- src/BallsSimulator.java
+++ src/BallsSimulator.java
@@ -4,34 +4,62 @@ import java.awt.Color;
 
 public class BallsSimulator implements Simulable {
     // Gère l'animation des balls
-    private Balls balles;
+    private Balls[] balles;
     private GUISimulator window;
+    private int[] dx;
+    private int[] dy;
 
     public BallsSimulator(Point[] ballons, GUISimulator window) {
-        this.balles = new Balls(ballons);
+        int nbBalles = ballons.length;
+        this.dx = new int[nbBalles];
+        this.dy = new int[nbBalles];
+        this.balles = new Balls[nbBalles];
+        for (int k =0; k < nbBalles; k++){
+            this.dx[k] = 5;
+            this.dy[k] = 5;
+            Point[] point = new Point[1];
+            point[0] = ballons[k];
+            this.balles[k] = new Balls(point);
+        }
         this.window = window;
         Afficher();
     }
 
     private void Afficher() {
         this.window.reset();
-        for (int i = 0; i < this.balles.getBalls().length; i++) {
+        for (int i = 0; i < this.balles.length; i++) {
             this.window.addGraphicalElement(
-            new Oval(this.balles.getBalls()[i].x, this.balles.getBalls()[i].y, Color.WHITE, Color.WHITE, 10));
+            new Oval(this.balles[i].getBalls()[0].x, this.balles[i].getBalls()[0].y, Color.WHITE, Color.WHITE, 10));
         }
     }
 
     @Override
     public void next() {
         // Bouge les balles de 5x et 8y
-        this.balles.translate(5, 8);
+        for (int i = 0; i < this.balles.length; i++) {
+            this.balles[i].translate(this.dx[i], this.dy[i]);
+            int x = this.balles[i].getBalls()[0].x;
+            int y = this.balles[i].getBalls()[0].y;
+            // Changer le 490 par la taille de la window - 10 (la moitie du cercle)
+            // Je sais pas comment la recuperer lire la doc
+            if (x < 0 || x > 490) {
+                this.dx[i] *= -1;
+                this.balles[i].translate(this.dx[i] * 2, 0);
+            }
+            if (y > 490 || y < 0) {
+                this.dy[i] *= -1;
+                this.balles[i].translate(0, this.dy[i] * 2);
+            }
+        }
         Afficher();
     }
 
     @Override
     public void restart() {
         // Remet les balles à son origine
-        this.balles.reInit();
+        for (int i = 0; i < this.balles.length; i++) {
+            this.balles[i].reInit();
+        }
         Afficher();
     }
 }
